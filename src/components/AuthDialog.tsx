0a9623c93fbce765a596d7427ccf075f74461074
Sharon <sharon.gomez@casebook.net>
2023-11-14 17:04:32 -0500

    remove double password, add a "show password" btn

--- src/components/AuthDialog.tsx
+++ src/components/AuthDialog.tsx
@@ -20,10 +20,9 @@ const AuthDialog = (props: AuthDialogProps) => {
 
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
-	const [confirmPassword, setConfirmPassword] = useState('')
 	const [emailRequired, setEmailRequired] = useState(false)
 	const [passwordRequired, setPasswordRequired] = useState(false)
-	const [passwordsMatch, setPasswordsMatch] = useState(true)
+	const [showPassword, setShowPassword] = useState(false)
 
 	useEffect(() => {
 		if (email) {
@@ -32,10 +31,7 @@ const AuthDialog = (props: AuthDialogProps) => {
 		if (password) {
 			setPasswordRequired(!password)
 		}
-		if (password && confirmPassword) {
-			setPasswordsMatch(password === confirmPassword)
-		}
-	}, [email, password, confirmPassword])
+	}, [email, password])
 
 	const handleCloseModal = () => {
 		setIsAuthDialogOpen(false)
@@ -48,18 +44,6 @@ const AuthDialog = (props: AuthDialogProps) => {
 		if (!email || !password) {
 			setEmailRequired(!email)
 			setPasswordRequired(!password)
-			return
-		}
-
-		if (!confirmPassword && !isLoggingIn) {
-			setPasswordRequired(!confirmPassword)
-			return
-		}
-
-		if (!isLoggingIn && !passwordsMatch) {
-			setError(true)
-			setMessage('Please make sure your passwords match')
-			setIsSnackbarOpen(true)
 			return
 		}
 
@@ -129,39 +113,50 @@ const AuthDialog = (props: AuthDialogProps) => {
 							</p>
 						)}
 
-						<input
-							type='password'
-							className='w-full p-3 mb-2 text-gray-700 border-0 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-blue-400 focus:bg-gray-50'
-							placeholder='password'
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
+						<div className='flex flex-row items-center w-full'>
+							<input
+								type={showPassword ? 'text' : 'password'}
+								className='w-full p-3 mb-2 text-gray-700 border-0 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-blue-400 focus:bg-gray-50'
+								placeholder='password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+							{showPassword ? (
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									viewBox='0 0 20 20'
+									fill='currentColor'
+									className='w-5 h-5 cursor-pointer ml-3 text-gray-400 hover:text-gray-500'
+									onClick={() => setShowPassword(false)}
+								>
+									<path
+										fillRule='evenodd'
+										d='M3.28 2.22a.75.75 0 00-1.06 1.06l14.5 14.5a.75.75 0 101.06-1.06l-1.745-1.745a10.029 10.029 0 003.3-4.38 1.651 1.651 0 000-1.185A10.004 10.004 0 009.999 3a9.956 9.956 0 00-4.744 1.194L3.28 2.22zM7.752 6.69l1.092 1.092a2.5 2.5 0 013.374 3.373l1.091 1.092a4 4 0 00-5.557-5.557z'
+										clipRule='evenodd'
+									/>
+									<path d='M10.748 13.93l2.523 2.523a9.987 9.987 0 01-3.27.547c-4.258 0-7.894-2.66-9.337-6.41a1.651 1.651 0 010-1.186A10.007 10.007 0 012.839 6.02L6.07 9.252a4 4 0 004.678 4.678z' />
+								</svg>
+							) : (
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									viewBox='0 0 20 20'
+									fill='currentColor'
+									className='w-5 h-5 cursor-pointer ml-3 text-gray-400 hover:text-gray-500'
+									onClick={() => setShowPassword(true)}
+								>
+									<path d='M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z' />
+									<path
+										fillRule='evenodd'
+										d='M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z'
+										clipRule='evenodd'
+									/>
+								</svg>
+							)}
+						</div>
 						{passwordRequired && (
 							<p className='text-red-400 text-sm ml-3 w-full'>
 								Please enter a password
 							</p>
-						)}
-
-						{!isLoggingIn && (
-							<>
-								<input
-									type='password'
-									className='w-full p-3 mb-2 text-gray-700 border-0 border-b border-gray-300 focus:outline-none focus:ring-0 focus:border-blue-400 focus:bg-gray-50'
-									placeholder='confirm password'
-									value={confirmPassword}
-									onChange={(e) => setConfirmPassword(e.target.value)}
-								/>
-								{passwordRequired && (
-									<p className='text-red-400 text-sm ml-3 w-full'>
-										Please enter a password
-									</p>
-								)}
-								{!passwordsMatch && (
-									<p className='text-red-400 text-sm ml-3 w-full'>
-										Passwords do not match
-									</p>
-								)}
-							</>
 						)}
 					</div>
 
