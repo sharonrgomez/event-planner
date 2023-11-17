f07b648b0185721df2430f0b1d77907258232e71
Sharon <sharon.gomez@casebook.net>
2023-11-16 20:29:16 -0500

    fix styles for months with < 4 weeks

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -1,4 +1,4 @@
-import {Dayjs} from 'dayjs'
+import dayjs, {Dayjs} from 'dayjs'
 import Day from './Day'
 
 import {getTotalWeeksInMonth} from '../utils/helpers'
@@ -11,10 +11,15 @@ type WeekProps = {
 const Week: React.FC<WeekProps> = (props: WeekProps) => {
 	const {week, weekIdx} = props
 
+	const totalWeeks = getTotalWeeksInMonth(
+		dayjs(week[0]).year(),
+		dayjs(week[0]).month(),
+	)
+	console.log('totalWeeks', totalWeeks)
 	return (
 		<div
-			className={`grid grid-cols-7 grid-rows-${getTotalWeeksInMonth}`}
-			style={{height: '150px'}}
+			className='grid grid-cols-7'
+			style={{height: `${totalWeeks === 4 ? '175px' : '150px'}`}}
 		>
 			{week.map((day, idx) => (
 				<Day day={day} weekIdx={weekIdx} key={idx} />
